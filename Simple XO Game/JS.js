bbd3ef1b485485c2831787cd319636b8f11a0160
Yazan F Al-Agan <150624834+YazanFAlagan@users.noreply.github.com>
2025-01-05 05:14:25 +0200

    Update JS.js

--- Simple XO Game/JS.js
+++ Simple XO Game/JS.js
@@ -1,19 +1,77 @@
-    let title = document.querySelector('.header');
-    let turn = 'x';    
+let title = document.querySelector('.header');
+    let turn = 'x'; 
+    let squares = [];   
+    let gameOver = false; 
+    function whoWins(num1, num2, num3) {
+        title.innerHTML = squares[num1] + ' Player Wins';
+        document.getElementById('item' + num1).style.backgroundColor = 'green';
+        document.getElementById('item' + num2).style.backgroundColor = 'green';
+        document.getElementById('item' + num3).style.backgroundColor = 'green';
+        
+        gameOver = true; 
+        
+        setInterval(function() { title.innerHTML += '.' }, 1000);
+        setTimeout(function() { location.reload() }, 3000);
+    }
 
-        function game(id) 
-        {
-            let element = document.getElementById(id);
-            if (turn === 'x' && element.innerHTML == '')
-            {
-                element.innerHTML = 'X';
-                turn = 'O';
-                title.innerHTML = 'O Player Turn';
+    function winner() {
+        for (let i = 1; i <= 9; i++) {
+            squares[i] = document.getElementById('item' + i).innerHTML;
+        }
+        if (squares[1] == squares[2] && squares[2] == squares[3] && squares[1] != '') {
+            whoWins(1, 2, 3);
+        } 
+        else if (squares[4] == squares[5] && squares[5] == squares[6] && squares[4] != '') {
+            whoWins(4, 5, 6);
+        } 
+        else if (squares[7] == squares[8] && squares[8] == squares[9] && squares[7] != '') {
+            whoWins(7, 8, 9);
+        } 
+        else if (squares[1] == squares[4] && squares[4] == squares[7] && squares[1] != '') {
+            whoWins(1, 4, 7);
+        } 
+        else if (squares[2] == squares[5] && squares[5] == squares[8] && squares[2] != '') {
+            whoWins(2, 5, 8);
+        } 
+        else if (squares[3] == squares[6] && squares[6] == squares[9] && squares[3] != '') {
+            whoWins(3, 6, 9);
+        } 
+        else if (squares[1] == squares[5] && squares[5] == squares[9] && squares[1] != '') {
+            whoWins(1, 5, 9);
+        } 
+        else if (squares[3] == squares[5] && squares[5] == squares[7] && squares[3] != '') {
+            whoWins(3, 5, 7);
+        } 
+        else {
+            let NonWins = true;
+            for (let i = 1; i <= 9; i++) {
+                if (squares[i] === '') {
+                    NonWins = false;
+                    break;
+                }
             }
-            else if (turn === 'O' && element.innerHTML == '')
-            {
-                element.innerHTML = 'O';
-                turn = 'X';
-                title.innerHTML = 'X Player Turn';
+            if (NonWins) {
+                title.innerHTML = "No One Wins";
+                setInterval(function() { title.innerHTML += '.' }, 1000);
+                gameOver = true; 
+                setTimeout(function() { location.reload(); }, 3000);
             }
         }
+    }
+
+    function game(id) {
+        if (gameOver) return; 
+        
+        let element = document.getElementById(id);
+        if (turn === 'x' && element.innerHTML == '') {
+            element.innerHTML = 'X';
+            turn = 'o';
+            title.innerHTML = 'O Player Turn';
+        } 
+        else if (turn === 'o' && element.innerHTML == '') {
+            element.innerHTML = 'O';
+            turn = 'x';
+            title.innerHTML = 'X Player Turn';
+        }
+        winner();
+    }
